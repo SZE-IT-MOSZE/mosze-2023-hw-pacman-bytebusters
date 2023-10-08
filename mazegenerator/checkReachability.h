5370b3216c30591075ca027509ba88ddec833046
pistixy <71815251+pistixy@users.noreply.github.com>
2023-10-08 18:19:48 +0200

    Delete mazegenerator directory

--- mazegenerator/checkReachability.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef CHECK_REACHABILITY_H
-#define CHECK_REACHABILITY_H
-
-#include <vector>
-
-bool checkReachability(const std::vector<std::vector<int>>& grid);
-
-#endif
--- mazegenerator/printGrid.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef PRINT_GRID_H
-#define PRINT_GRID_H
-
-#include <vector>
-
-void printGrid(const std::vector<std::vector<int>>& grid);
-
-#endif
