c20ba2d8ea542cb924be8de8c45cb2e852633ca3
pistixy <71815251+pistixy@users.noreply.github.com>
2023-10-15 09:12:06 +0200

    Collectibles on the map

--- ByteBusters/Game/Mazegenerator/main.cpp
+++ ByteBusters/Game/Mazegenerator/main.cpp
@@ -7,48 +7,36 @@
 #include "removeEdgeAdjacentBlocks.h"
 #include "writeGridToFile.h"
 #include "checkReachability.h"
+#include "placeTwos.cpp"
 
 int main() {
-    std::string folderPath = "D:/Sulesz/Mosze/Maze/mazegenerator/mazegenerator/fields";
+    std::string folderPath = "D:/Sulesz/Mosze/ByteBusters/Game/Mazegenerator/fields";
     int files = 1;
-    while (files != 21) {
-        int a = 35; // Meret beallitasa
-        int b = 28;
-        float targetDensity = 0.265; //suruseg beallitasa
-        std::string filename = "grid_output.txt"; // mi legyen a kimenti fajlok neve? inicializalas?, 1 filehoz kellett, 20, hoz annyira nem, de enelkul a sor nelkul nem mukodik nem ertem miert lol
-
-        //tomb generalasa 00000
-        //                00000
-        //                00000
+    int a = 35; 
+    int b = 28;
+    double targetDensity = 0.2; 
+    while (files <= 2) {
         std::vector<std::vector<int>> grid = createGrid(a, b);
 
-        // szelek 1-esse valtozatasa
-        modifyGridEdges(grid);
-
-        // tetris blokkok lerakasa a suruseg elereseig
-        placeTetrisBlocks(grid, targetDensity);
-
-        // a szelek melletti folyoso kialakitasa
+        placeTetrisBlocks(grid,targetDensity);
         removeEdgeAdjacentBlocks(grid);
+        
+        
+        std::ostringstream filenameStream;
+        filenameStream << folderPath << "\\" << files << ".txt";
+        std::string filename = filenameStream.str();
 
-        // nezzuk meg, hogy minden 0 elerheto e
         bool isReachable = checkReachability(grid);
-
-        // ujra legyen minden szelso blokk 1-es, hatha megvaltozott
-        modifyGridEdges(grid);
-
-        // kiiras teszteleshez
-        //printGrid(grid);
-
-        // irjuk ki fileokba
         if (isReachable) {
-            std::ostringstream filenameStream;
-            filenameStream << folderPath << "/grid" << files << ".txt";
-            filename = filenameStream.str();
+            modifyGridEdges(grid);
+            placeTwos(grid);
+            std::cout << "Grid: " << files << std::endl;
+            printGrid(grid);
             writeGridToFile(grid, filename);
             std::cout << "Saved grid " << files << " to " << filename << std::endl;
             files++;
         }
+
     }
     return 0;
 }
