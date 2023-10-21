5bf6e841adfc5c8679711a2704cff14965f6e830
pistixy <71815251+pistixy@users.noreply.github.com>
2023-10-21 18:05:34 +0200

    Delete ByteBusters directory

--- ByteBusters/Game/Mazegenerator/createGrid.cpp
+++
@@ -1,7 +0,0 @@
-#include "createGrid.h"
-
-std::vector<std::vector<int>> createGrid(int a, int b) {
-    // 2dimenzios vektor a grid
-    std::vector<std::vector<int>> grid(a, std::vector<int>(b, 0));
-    return grid;
-}
