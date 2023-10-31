25d48f9f15abf4d7395c778e7de2445fb93975dd
Tárnok Ákos <tarnokakos@live.com>
2023-10-31 13:37:49 +0100

    Kommentelés Doxygenhez 2
    Kommentelés folytatása Doxygen dokumentációhoz

--- ByteBusters/Game/MazeGenerator/Source Files/modifyGridEdges.cpp
+++ ByteBusters/Game/MazeGenerator/Source Files/modifyGridEdges.cpp
@@ -1,17 +1,17 @@
 #include "modifyGridEdges.h"
 
 void modifyGridEdges(std::vector<std::vector<int>>& grid) {
-    // meret lekerdezese
+    //!< A méret lekérdezése
     int numRows = grid.size();
     int numCols = grid[0].size();
-    // Also es felsosorok kinullasaza
+    //! Az also és felsõsorok kinullázása
     for (int col = 0; col < numCols; col++) {
-        grid[0][col] = 1;                // felso
-        grid[numRows - 1][col] = 1;     // also
+        grid[0][col] = 1;                //!< A felsõ sor
+        grid[numRows - 1][col] = 1;     //!< Az alsó sor
     }
-    // bal es jobb oldal kinullazasa
+    //!< A bal es jobb oldal kinullázása
     for (int row = 1; row < numRows - 1; row++) {
-        grid[row][0] = 1;                // bal
-        grid[row][numCols - 1] = 1;     // jobb
+        grid[row][0] = 1;                //!< A bal oldal
+        grid[row][numCols - 1] = 1;     //!< A jobb oldal
     }
 }
--- ByteBusters/Game/MazeGenerator/Source Files/placeTetrisBlocks.cpp
+++ ByteBusters/Game/MazeGenerator/Source Files/placeTetrisBlocks.cpp
@@ -2,19 +2,19 @@
 #include <cstdlib> 
 #include <ctime>   
 //blokk alakok kikialtasa
-const std::vector<std::vector<int>> tetrisShapes[] = {
+const std::vector<std::vector<int>> tetrisShapes[] = { //!< A blokk alakok kialakítása ami az adott pályán szerepelni fognak
     // L alak
-    {{1, 0}, {1, 0}, {1, 1}},
+    {{1, 0}, {1, 0}, {1, 1}}, //!< L alakú blokk
     // T alak
-    {{1, 1, 1}, {0, 1, 0}},
+    {{1, 1, 1}, {0, 1, 0}}, //!< T alakú blokk
     // negyzet alak
-    {{1, 1}, {1, 1}},
+    {{1, 1}, {1, 1}}, //!< Négyzet alakú blokk
     // egyenes
-    {{1, 1, 1, 1}},
+    {{1, 1, 1, 1}}, //!< Egyenes alakú blokk
 };
 
 void placeTetrisBlocks(std::vector<std::vector<int>>& grid, float targetDensity) {
-    // meret lekerese
+    //!< A méret lekérése
     int numRows = grid.size();
     int numCols = grid[0].size();
 
@@ -22,20 +22,20 @@ void placeTetrisBlocks(std::vector<std::vector<int>>& grid, float targetDensity)
     //std::srand(static_cast<unsigned int>(std::time(nullptr)));
 
     //szamoljuk ki hany cellat kell feltolteni
-    int maxCells = static_cast<int>(targetDensity * numRows * numCols);
+    int maxCells = static_cast<int>(targetDensity * numRows * numCols); //!< Annak a számolása, hogy mennyi cellát kell feltöltenünk
 
-    int filledCells = 0; //0 feltott cellaval kezdunk
+    int filledCells = 0; //!< Kezdetben 0-a feltöltött cella van
 
-    while (filledCells < maxCells) { //feltoltes
-        // veletlenszeruen valaszt alakit
-        const std::vector<std::vector<int>>& shape = tetrisShapes[std::rand() % 4];
+    while (filledCells < maxCells) { //!< A feltöltes
+        
+        const std::vector<std::vector<int>>& shape = tetrisShapes[std::rand() % 4]; //!<  Veletlenszerüen választunk  alakzatot
 
         // veletlenszeruen valaszt helyet
         int row = std::rand() % (numRows - shape.size() + 1);
         int col = std::rand() % (numCols - shape[0].size() + 1);
 
         // nezuk meg, ide lerakhatja e
-        bool canPlace = true;
+        bool canPlace = true; //!< Azt nézzük meg, hogy az adott helyre letehetõ-e a választott blokk
         for (size_t i = 0; i < shape.size(); i++) {
             for (size_t j = 0; j < shape[i].size(); j++) {
                 if (shape[i][j] == 1 && grid[row + i][col + j] == 1) {
@@ -49,7 +49,7 @@ void placeTetrisBlocks(std::vector<std::vector<int>>& grid, float targetDensity)
         }
 
         // elhelyezes
-        if (canPlace) {
+        if (canPlace) { //!< Az adott blokk elhelyezése a pályán 
             for (size_t i = 0; i < shape.size(); i++) {
                 for (size_t j = 0; j < shape[i].size(); j++) {
                     if (shape[i][j] == 1) {
