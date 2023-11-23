bf920e1b35d81bfab5478f614a39caa13ac28a30
Tárnok Ákos <tarnokakos@live.com>
2023-11-23 22:32:45 +0100

    Teszt
    Tesztelésre való előkészülés.

---
+++ ByteBusters/Game/Test/test.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+
+TEST() { // elsõ teszt eset lesz
+}
+
+TEST() { // második teszt eset lesz
+}
+
+TEST() { // harmadik teszt eset lesz
+}
+
+TEST() { // negyedik teszt eset lesz
+}
+
+TEST() { // ötödik teszt eset lesz
+}
+
